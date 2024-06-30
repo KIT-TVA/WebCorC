0f7214995946f9cf6083db58b8d70b138d1e33cd
uulud <uulud@student.kit.edu>
2024-06-30 21:30:27 +0200

    Cleanup before creating new Project structure

--- src/main/java/edu/kit/cbc/web/util/ProofType.java
+++
@@ -1,9 +0,0 @@
-package edu.kit.cbc.web.util;
-
-public enum ProofType {
-	// These are use to differ between proof types in VerifyAllStatements.proveStatement
-	Variant,
-	Postcondition,
-	Precondition,
-	FullProof;
-}
