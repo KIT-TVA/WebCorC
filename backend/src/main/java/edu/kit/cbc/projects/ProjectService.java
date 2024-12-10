2642f827e2538a98d722b2f298385391732b600e
Medath <40150792+Medath@users.noreply.github.com>
2024-12-10 17:54:53 +0100

    Refactor file management and add todos

--- backend/src/main/java/edu/kit/cbc/projects/ProjectService.java
+++ backend/src/main/java/edu/kit/cbc/projects/ProjectService.java
@@ -1,5 +1,7 @@
 package edu.kit.cbc.projects;
 
+import java.net.URI;
+
 import jakarta.validation.constraints.NotBlank;
 
 public interface ProjectService {
@@ -7,5 +9,8 @@ public interface ProjectService {
     ReadProjectDto updateById(@NotBlank String id, ReadProjectDto project);
     ReadProjectDto updateById(@NotBlank String id, CreateProjectDto project);
     ReadProjectDto findById(@NotBlank String id);
+    boolean existsById(@NotBlank String id);
+    void addFilePathToId(@NotBlank String id, URI uri);
+    void removeFilePathFromId(@NotBlank String id, URI uri);
     void deleteById(@NotBlank String id);
 }
