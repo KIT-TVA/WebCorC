e7d58b76c3092ab47a9afe8b692afd8accf66954
uulud <uulud@student.kit.edu>
2025-01-21 00:24:46 +0100

    Add first tests to the app component
    Signed-off-by: uulud <uulud@student.kit.edu>

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -2,8 +2,16 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { HarnessLoader } from '@angular/cdk/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+import {MatButtonHarness} from '@angular/material/button/testing';
+import {MatDialogHarness} from '@angular/material/dialog/testing';
+import { ProjectService } from './services/project/project.service';
 
 describe('AppComponent', () => {
+
+  let loader : HarnessLoader
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
@@ -11,6 +19,7 @@ describe('AppComponent', () => {
     }).compileComponents();
 
     const fixture = TestBed.createComponent(AppComponent)
+    loader = TestbedHarnessEnvironment.loader(fixture)
     fixture.autoDetectChanges()
     const component = fixture.componentInstance
   });
@@ -21,10 +30,35 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should initalize with disabled generate content and verify buttons', () => {
+  it('should initalize with disabled generate code and verify buttons', async () => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.autoDetectChanges()
+    const generateCodeButton = await loader.getHarnessOrNull(MatButtonHarness.with({ text: 'code  Generate Code' }))
+    expect(generateCodeButton).not.toBe(null)
+    expect(await generateCodeButton?.isDisabled()).toBeTrue()
+
+    const verifyButton = await loader.getHarnessOrNull(MatButtonHarness.with({ text: 'check_circle Verify'}))
+    expect(verifyButton).not.toBe(null)
+    expect(await verifyButton?.isDisabled()).toBeTrue()
+
+  })
+
+  it('should open "Create Project" Dialog on click on Share button without project id not set', async () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.autoDetectChanges()
+
+    const shareButton = await loader.getHarnessOrNull(MatButtonHarness.with({ text : 'share  Share'}))
+    expect(shareButton).not.toBe(null)
+    
+    const projectService = fixture.debugElement.injector.get(ProjectService)
+    expect(projectService.projectId).toBe(undefined)
+
+    /*shareButton?.click()
     fixture.detectChanges()
-    const compiled = fixture.nativeElement as HTMLElement
-    expect(compiled.querySelector('mat-toolbar > button'))
+
+    const dialogs = await loader.getAllHarnesses(MatDialogHarness)
+    console.log(dialogs)
+    expect(dialogs.length).toBe(1)
+    expect(await dialogs[0].getTitleText() == 'Create Project')*/
   })
 });
