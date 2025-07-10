e4a692f51c3740ff8487f0bd21ec4f75ddf150e6
Medath <40150792+Medath@users.noreply.github.com>
2025-07-10 16:19:49 +0200

    Autogenerate OpenAPI Specification and Swagger UI for backend
    Accessible from /swagger-ui

--- backend/src/main/java/edu/kit/cbc/Application.java
+++ backend/src/main/java/edu/kit/cbc/Application.java
@@ -1,6 +1,14 @@
 package edu.kit.cbc;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "WebCorC API"
+    )
+)
 
 public class Application {
 
