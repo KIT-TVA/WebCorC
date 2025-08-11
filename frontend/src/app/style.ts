337cc13f44ca8dba3e777ed7187173d748dd4cf8
Cián <uezbf@student.kit.edu>
2025-08-12 01:07:22 +0200

    Update colour scheme to conform to TVA identity

--- frontend/src/app/style.ts
+++ frontend/src/app/style.ts
@@ -3,19 +3,6 @@ import Aura from "@primeuix/themes/aura";
 
 export const Noir = definePreset(Aura, {
   semantic: {
-    primary: {
-      50: "{zinc.50}",
-      100: "{zinc.100}",
-      200: "{zinc.200}",
-      300: "{zinc.300}",
-      400: "{zinc.400}",
-      500: "{zinc.500}",
-      600: "{zinc.600}",
-      700: "{zinc.700}",
-      800: "{zinc.800}",
-      900: "{zinc.900}",
-      950: "{zinc.950}",
-    },
     colorScheme: {
       light: {
         surface: {
@@ -34,14 +21,14 @@ export const Noir = definePreset(Aura, {
         },
         background: "{surface.100}",
         primary: {
-          color: "{zinc.950}",
+          color: "{green.900}",
           inverseColor: "#ffffff",
-          hoverColor: "{zinc.900}",
-          activeColor: "{zinc.800}",
+          hoverColor: "{green.800}",
+          activeColor: "{green.700}",
         },
         highlight: {
-          background: "{zinc.950}",
-          focusBackground: "{zinc.700}",
+          background: "{green.950}",
+          focusBackground: "{green.700}",
           color: "#ffffff",
           focusColor: "#ffffff",
         },
@@ -63,16 +50,16 @@ export const Noir = definePreset(Aura, {
         },
         background: "{surface.800}",
         primary: {
-          color: "{zinc.50}",
+          color: "{green.500}",
           inverseColor: "{zinc.950}",
-          hoverColor: "{zinc.100}",
-          activeColor: "{zinc.200}",
+          hoverColor: "{green.300}",
+          activeColor: "{green.200}",
         },
         highlight: {
-          background: "rgba(250, 250, 250, .16)",
-          focusBackground: "rgba(250, 250, 250, .24)",
-          color: "rgba(255,255,255,.87)",
-          focusColor: "rgba(255,255,255,.87)",
+          background: "{green.950}",
+          focusBackground: "{green.700}",
+          color: "#ffffff",
+          focusColor: "#ffffff",
         },
       },
     },
