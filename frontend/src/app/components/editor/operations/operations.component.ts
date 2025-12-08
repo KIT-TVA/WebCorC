507f3eb45664dba522cb3b92c8baee952074f360
JaferShar <jafer.shar@gmail.com>
2025-12-08 15:08:53 +0100

    added sidemenu tab >operations< with >reset< as button

---
+++ frontend/src/app/components/editor/operations/operations.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {Button} from "primeng/button";
+
+@Component({
+  selector: 'app-operations',
+  imports: [
+    Button
+  ],
+  templateUrl: './operations.component.html',
+  styleUrl: './operations.component.scss'
+})
+export class OperationsComponent {
+
+  resetGraph() {
+
+  }
+}
