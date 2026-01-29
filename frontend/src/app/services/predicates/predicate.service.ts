57df85da04e64a8980afebf6bc3ee15258eabe71
Cián Payne <uezbf@student.kit.edu>
2026-01-29 03:30:48 +0100

    Improve code of predicate manager

--- frontend/src/app/services/predicates/predicate.service.ts
+++ frontend/src/app/services/predicates/predicate.service.ts
@@ -7,18 +7,19 @@ import { ProjectService } from "../project/project.service";
 })
 export class PredicateService {
   constructor(private projectService: ProjectService) {}
-  private idCounter = 2;
+  private idCounter = 0;
   private predicates: ProjectPredicate[] = [];
   public getPredicates() {
     return this.predicates;
   }
   public addPredicate() {
     const newPredicate: ProjectPredicate = {
-      id: String(this.idCounter++),
-      name: "untitled predicate " + this.idCounter / 2,
+      id: String(this.idCounter),
+      name: "untitled predicate " + this.idCounter,
       definition: "",
       signature: "",
     };
+    this.idCounter++;
     this.predicates.push(newPredicate);
     return newPredicate;
   }
