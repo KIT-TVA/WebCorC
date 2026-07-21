260301010c8e985da1d47393ff59f3446b7b967e
S3vro <sevro@gmx.de>
2026-07-21 13:52:53 +0200

    work: added rate-limit for ai endpoint

--- backend/src/main/java/edu/kit/cbc/common/corc/parsing/SemanticException.java
+++ backend/src/main/java/edu/kit/cbc/common/corc/parsing/SemanticException.java
@@ -2,7 +2,14 @@ package edu.kit.cbc.common.corc.parsing;
 
 public class SemanticException extends Exception {
 
+    private final String variableName;
+
     public SemanticException(String message) {
         super(message);
+        this.variableName = null;
+    }
+
+    public String getVariableName() {
+        return variableName;
     }
 }
